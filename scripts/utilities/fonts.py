6c174b4151f45726e7c542fc4679414c38f1a479
Chris Simpkins <git.simpkins@gmail.com>
2016-01-24 12:07:04 -0500

    new font list script

---
+++ scripts/utilities/fonts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+DEFAULT_SIZE = "14"
+
+font_list = [
+    ["Anonymous Pro", {'size': DEFAULT_SIZE, 'filename': 'anonymous-pro'}],
+    ["Aurulent Sans Mono", {'size': DEFAULT_SIZE, 'filename': 'aurulent-sans-mono'}],
+    ["AverageMono", {'size': DEFAULT_SIZE, 'filename': 'average-mono'}],
+    ["Bitstream Vera Sans Mono", {'size': DEFAULT_SIZE, 'filename': 'bitstream-vera-sans-mono'}],
+    ["BPmono", {'size': DEFAULT_SIZE, 'filename': 'bp-mono'}],
+    ["Bront", {'size': DEFAULT_SIZE, 'filename': 'bront'}],
+    ["Camingo Code", {'size': DEFAULT_SIZE, 'filename': 'camingo-code'}],
+    ["Dina ttf 10px", {'size': '10', 'filename': 'dina'}]
+]
