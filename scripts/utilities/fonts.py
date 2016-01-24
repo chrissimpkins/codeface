95e157bc738f450c19b77280a66ff0cb16b685ec
Chris Simpkins <git.simpkins@gmail.com>
2016-01-24 17:42:44 -0500

    updated fonts

--- scripts/utilities/fonts.py
+++ scripts/utilities/fonts.py
@@ -11,5 +11,20 @@ font_list = [
     ["BPmono", {'size': DEFAULT_SIZE, 'filename': 'bp-mono'}],
     ["Bront", {'size': DEFAULT_SIZE, 'filename': 'bront'}],
     ["CamingoCode", {'size': DEFAULT_SIZE, 'filename': 'camingo-code'}],
-    ["Dina ttf 10px", {'size': '10', 'filename': 'dina'}]
+    ["CMU Typewriter Text", {'size': DEFAULT_SIZE, 'filename': 'cmu-typewriter'}],
+    ["Code New Roman", {'size': DEFAULT_SIZE, 'filename': 'code-new-roman'}],
+    ["CodingFontTobi", {'size': '16', 'filename': 'coding-font-tobi'}],
+    ["Consola Mono", {'size': DEFAULT_SIZE, 'filename': 'consola-mono'}],
+    ["Courier Prime Code", {'size': DEFAULT_SIZE, 'filename': 'courierprime-code'}],
+    ["Cousine", {'size': DEFAULT_SIZE, 'filename': 'cousine'}],
+    ["CPMono_v07", {'size': DEFAULT_SIZE, 'filename': 'cp-mono'}],
+    ["Crisp", {'size': DEFAULT_SIZE, 'filename': 'crisp'}],
+    ["Cutive Mono", {'size': DEFAULT_SIZE, 'filename': 'cutive-mono'}],
+    ["DejaVu Sans Mono", {'size': DEFAULT_SIZE, 'filename': 'dejavu-sans-mono'}],
+    ["Dina ttf 10px", {'size': '10', 'filename': 'dina'}],
+    ["Droid Sans Mono", {'size': DEFAULT_SIZE, 'filename': 'droid-sans-mono'}],
+    ["Edlo", {'size': DEFAULT_SIZE, 'filename': 'edlo'}],
+    ["Fantasque Sans Mono", {'size': DEFAULT_SIZE, 'filename': 'fantasque-sans-mono'}],
+    ["Fira Code", {'size': DEFAULT_SIZE, 'filename': 'fira-code'}],
+    ["Fira Mono", {'size': DEFAULT_SIZE, 'filename': 'fira-mono'}]
 ]
