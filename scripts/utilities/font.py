d1b3603d676a96b8516de2e04e535efc0e52a77b
Chris Simpkins <git.simpkins@gmail.com>
2016-02-26 17:16:58 -0500

    new nondistrib gallery images

--- scripts/utilities/font.py
+++ scripts/utilities/font.py
@@ -1,5 +1,10 @@
 DEFAULT_SIZE = "14"
 
 font_list = [
-    ["Cousine", {'size': DEFAULT_SIZE, 'filename': 'cousine'}]
+    ["Envy Code R", {'size': DEFAULT_SIZE, 'filename': 'envy-code-r'}],
+    ["Input Mono", {'size': DEFAULT_SIZE, 'filename': 'input-mono'}],
+    ["Klartext Mono", {'size': DEFAULT_SIZE, 'filename': 'klartext-mono'}],
+    ["Monofonto", {'size': DEFAULT_SIZE, 'filename': 'monofonto'}],
+    ["Monoxil", {'size': DEFAULT_SIZE, 'filename': 'monoxil'}],
+    ["NK57 Monospace", {'size': DEFAULT_SIZE, 'filename': 'nk57-monospace'}]
 ]
