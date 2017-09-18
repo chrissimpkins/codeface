ce30f44d6e81fee16aa6cf5cb6775f9bfcab9ab0
Chris Simpkins <git.simpkins@gmail.com>
2017-09-17 20:13:32 -0400

    update nondistrib gallery images

--- scripts/utilities/font.py
+++ scripts/utilities/font.py
@@ -1,5 +1,10 @@
 DEFAULT_SIZE = "14"
 
 font_list = [
-    ["Generic Mono II", {'size': DEFAULT_SIZE, 'filename': 'generic'}],
+    ["Input Mono", {'size': DEFAULT_SIZE, 'filename': 'input-mono'}],
+    ["Envy Code R", {'size': DEFAULT_SIZE, 'filename': 'envy-code-r'}],
+    ["Klartext Mono", {'size': DEFAULT_SIZE, 'filename': 'klartext-mono'}],
+    ["Monofonto", {'size': DEFAULT_SIZE, 'filename': 'monofonto'}],
+    ["Monoxil Regular", {'size': DEFAULT_SIZE, 'filename': 'monoxil'}],
+    ["NK57 Monospace", {'size': DEFAULT_SIZE, 'filename': 'nk57-monospace'}]
 ]
